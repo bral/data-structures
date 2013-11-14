73e1695b6488a56836eea4140ece8e7593e45d9b
brannonlucas <brannonlucas@gmail.com>
2013-11-13 21:43:52 -0800

    implements stack in pseudoclassical style

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,23 @@
 var Stack = function() {
+
+  this.storage = {};
+  this.length = 0;
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
 };
+
+Stack.prototype.size = function(){
+  return this.length;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Stack.prototype.pop = function(){
+  var last = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length && this.length--;
+  return last;
+};
+
