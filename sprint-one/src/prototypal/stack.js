782af2e80975fa2cf015a5182afee8cb56eaa92d
brannonlucas <brannonlucas@gmail.com>
2013-11-13 21:21:52 -0800

    implements stack operations by delegating to stackMethods object.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,28 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.length = 0;
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  },
+
+  pop: function(){
+    var last = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length && this.length--;
+    return last;
+  },
+
+  size: function(){
+    console.log('stackMethod',this.length);
+    return this.length;
+  }
+};
+
+
