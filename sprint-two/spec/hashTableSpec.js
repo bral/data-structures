b67c7a61e2d586880bc909f09af387b6b05a7ae4
brannonlucas <brannonlucas@gmail.com>
2013-11-15 12:44:22 -0800

    hashTables uses linked lists to store entries.

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -10,9 +10,48 @@ describe("hashTable", function() {
     expect(hashTable.retrieve).toEqual(jasmine.any(Function));
   });
 
+  it("should be able to insert and retrieve a value", function() {
+    hashTable.insert("one", 1);
+    expect(hashTable.retrieve("one")).toEqual(1);
+  });
+
+  it("should be able to remove a value when referenced by a key", function() {
+    hashTable.insert("one", 1);
+    hashTable.remove("one");
+    expect(hashTable.retrieve("one")).toEqual(null);
+  });
+
+  it("should create a linkedList when makeLinkedList is called", function() {
+    var ll = hashTable.makeLinkedList();
+    expect(ll.head).toEqual(null);
+  });
+
+  it("should create a linkedList and add new value on insert invocation if no value is present at specified index", function() {
+    hashTable.insert("one", 1);
+    expect(hashTable.retrieve("one").head.value).toEqual(1);
+  });
+
+  it("should add value to a pre-exsiting linkedList", function() {
+    hashTable.insert("one", 1);
+    hashTable.insert("one", 2);
+    expect(hashTable.retrieve("one").tail.value).toEqual(2);
+    expect(hashTable.retrieve("one").head.value).toEqual(1);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
   it("should handle hash function collisions", function(){
     // force the hash function to return 0
-    spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
+    //spyOn(window, 'getIndexBelowMaxForKey').andReturn(0);
     var v1 = 'val1', v2 = 'val2';
     hashTable.insert(v1, v1);
     hashTable.insert(v2, v2);
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -14,8 +14,18 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var entries;
+
   console.log(i);
-  this._storage.set(i, v);
+
+  if (this._storage.get(i) === undefined) {
+    entries = this.makeLinkedList()
+    entries.addToTail(v);
+    this._storage.set(i, entries);
+  } else {
+    entries = this._storage.get(i);
+    entries.addToTail(v);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -23,7 +33,68 @@ HashTable.prototype.retrieve = function(k){
   return this._storage.get(i);
 };
 
-HashTable.prototype.remove = function(){
+HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
+};
+
+HashTable.prototype.makeLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value){
+    var node = makeNode(value);
+    if (this.head === null && this.tail === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  };
+
+  list.removeHead = function(){
+    var firstValue;
+
+    if (this.head === this.tail) {
+      firstValue = this.head && this.head.value
+      this.head = null;
+      this.tail = null;
+      return firstValue;
+    }
+
+    firstValue = this.head.value;
+    this.head = this.head.next;
+    return firstValue;
+  };
+
+  list.contains = function(item){
+    var node = this.head;
+
+    if(!node) {
+      return false;
+    }
+
+    while (node.next) {
+      if (node.value === item) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
+  };
+
+  return list;
+  };
+
+  var makeNode = function(value){
+  var node = {};
+  node.value = value;
+  node.next = null;
+
+  return node;
 };
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
