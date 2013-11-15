0ebec42911e9cc0acbd42800a030bf9a2c4b0c52
brannonlucas <brannonlucas@gmail.com>
2013-11-14 16:26:40 -0800

    adds child node to existing child node, returns true if target value is found on target.

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -11,5 +11,26 @@ describe("tree", function() {
     expect('value' in tree).toBe(true);
   });
 
-  // Add more tests here to test the functionality of tree.
+  it("should have property named 'children' that is of type array", function() {
+    expect(Array.isArray(tree.children)).toEqual(true);
+  });
+
+  it("should increase length of children array by one when addChild is invoked", function() {
+    var treeLength = tree.children.length
+    tree.addChild("x");
+    tree.addChild("y");
+    tree.addChild("z");
+    expect(tree.children.length).toEqual(treeLength + 3);
+  })
+
+  it("should add a child node to a child node", function() {
+    tree.addChild("child");
+    tree.children[0].addChild("grand child");
+    expect(tree.children[0].children.length).not.toEqual(0);
+  });
+
+  it("should return true if target value is found on target", function() {
+    tree.value = 12;
+    expect(tree.contains(12)).toEqual(true);
+  });
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,24 @@
 var makeTree = function(){
   var newTree = {};
   newTree.value = undefined;
-  newTree.children = undefined;
+  newTree.children = [];
+
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  // _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  var thisChild = makeTree();
+  thisChild.value = value;
+  this.children.push(thisChild);
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(value){
+  return this.value === value;
 };
